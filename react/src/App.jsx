934690225252c9ec89639d7bb4f62df623e84e70
iamsiva03 <sivakarnan03@gmail.com>
2026-02-19 15:35:16 +0530

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+
+function App() {
+
+//spread operator
+const first = [1, 2, 3]
+const second = [...first, 4, 5, 6] // we can use the spread operator to combine two arrays
+
+const fruit = ['apple', 'banana', 'orange'] // map function needs a key, so we can use the item itself as the key
+  
+  // classes
+  class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+    }
+
+    greet() {
+      return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+  }
+  const p = new Person("Siva", 20)
+
+  // Destructuring
+
+  const [a, b, c] = first 
+
+  return (
+
+    <div>
+    <ul>
+      {fruit.map((item) => <li key={item}>{item}</li>)}
+    </ul>
+    <p>First array: {first}</p><p>Second array: {second}</p>
+
+    <p>Person: {p.name}, Age: {p.age}</p>
+    <p>{p.greet()}</p>
+
+    <p>Destructured values: a={a}, b={b}, c={c}</p>
+</div>
+  )
+}
+
+export default App
